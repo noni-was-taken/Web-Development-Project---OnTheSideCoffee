9d50c931e51fd23757612047403320b085abcc81
Desmond823 <jb.machacon@gmail.com>
2025-05-23 19:52:47 +0800

    login function implementd

--- html/adminLogin.php
+++ html/adminLogin.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
 
-    <title>Document</title>
+    <title>Login</title>
 
     <link rel="stylesheet" href="../css/adminLogin.css">
 </head>
